ef9d611d5d73dff983f9dc1888f75844a06f176d
abiserov <abiserov@team.amocrm.com>
2023-10-08 06:51:22 +0300

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Александр Бисеров on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
